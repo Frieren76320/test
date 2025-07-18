57c6de8f0064f2880fe5fcc594e1078063970668
evans <heltorkeltor50gmail.com>
2025-07-18 15:43:49 +0200

    ajout fichier variables

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "acr_server" {
+  description = "The Azure Container Registry server URL"
+  type        = string
+}
+ 
+variable "acr_username" {
+  description = "The Azure Container Registry username"
+  type        = string
+}
+ 
+variable "acr_password" {
+  description = "The Azure Container Registry password"
+  type        = string
+}
